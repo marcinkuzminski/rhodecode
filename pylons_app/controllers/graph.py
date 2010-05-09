6c769062f5ce119fa76ec0991dda58de47770ec8
Marcin Kuzminski <marcin@python-works.com>
2010-05-09 14:46:54 +0200

    fixed graph when repository is empty moved imports to top and removed some lfet over prints

--- pylons_app/controllers/graph.py
+++ pylons_app/controllers/graph.py
@@ -1,11 +1,18 @@
+import logging
 from pylons import request, response, session, tmpl_context as c, url, config, \
     app_globals as g
 from pylons.controllers.util import abort, redirect
 from pylons_app.lib.base import BaseController, render
 from pylons_app.lib.utils import get_repo_slug
 from pylons_app.model.hg_model import HgModel
-import logging
 
+from mercurial.graphmod import revisions as graph_rev, colored, CHANGESET
+from pylons_app.lib.filters import age as _age, person
+from simplejson import dumps
+from mercurial.node import short
+from webhelpers.paginate import Page
+        
+        
 
 log = logging.getLogger(__name__)
 
@@ -27,14 +34,11 @@ class GraphController(BaseController):
 
 
     def graph(self, repo, size):
-        from mercurial.graphmod import revisions as graph_rev, colored, CHANGESET
-        from pylons_app.lib.filters import age as _age, person
-        from simplejson import dumps
-        from mercurial.node import short
-        from webhelpers.paginate import Page
         revcount = size
         p = int(request.params.get('page', 1))
         c.pagination = Page(repo.revisions, page=p, item_count=len(repo.revisions), items_per_page=revcount)
+        if not repo.revisions:return dumps([]), 0
+        
         max_rev = repo.revisions[-1]
         offset = 1 if p == 1 else  ((p - 1) * revcount)
         rev_start = repo.revisions[(-1 * offset)]
@@ -42,8 +46,6 @@ class GraphController(BaseController):
         
         revcount = min(max_rev, revcount)
         rev_end = max(0, rev_start - revcount)
-        print rev_start, rev_end
-        print 'x' * 100
         dag = graph_rev(repo.repo, rev_start, rev_end)
         tree = list(colored(dag))
         canvasheight = (len(tree) + 1) * bg_height - 27
