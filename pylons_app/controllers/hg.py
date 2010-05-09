c1fa5cebf8100523e5153721b7d31168413ec834
Marcin Kuzminski <marcin@python-works.com>
2010-05-09 13:20:52 +0200

    removed ununsed imports

--- pylons_app/controllers/hg.py
+++ pylons_app/controllers/hg.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from mako.template import Template
-from mercurial.hg import repository
-from mercurial.hgweb import hgweb
-from mercurial.hgweb.request import wsgiapplication
-from mercurial.localrepo import localrepository
+import logging
 from operator import itemgetter
-from pylons import tmpl_context as c, app_globals as g, session, request, config
-from pylons.controllers.util import abort
-from pylons_app.lib import helpers as h
+from pylons import tmpl_context as c, request, config
 from pylons_app.lib.base import BaseController, render
 from pylons_app.lib.utils import get_repo_slug
 from pylons_app.model.hg_model import HgModel
-import logging
-import os
 from beaker.cache import cache_region
 log = logging.getLogger(__name__)
 
