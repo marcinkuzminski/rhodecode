28121d3550c8f259fb12b55d6449cef450beb4aa
Marcin Kuzminski <marcin@python-works.com>
2010-05-16 15:06:20 +0200

    Added colored formatter to project, and configs

--- pylons_app/lib/timerproxy.py
+++ pylons_app/lib/timerproxy.py
@@ -2,6 +2,28 @@ from sqlalchemy.interfaces import ConnectionProxy
 import time
 import logging
 log = logging.getLogger('timerproxy')
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = xrange(30, 38)
+
+def color_sql(sql):
+    COLOR_SEQ = "\033[1;%dm"
+    COLOR_SQL = YELLOW
+    normal = '\x1b[0m'
+    return COLOR_SEQ % COLOR_SQL + sql + normal 
+
+def format_sql(sql):
+    sql = color_sql(sql)
+    sql = sql.replace('SELECT', '\n    SELECT \n\t')\
+        .replace('FROM', '\n    FROM')\
+        .replace('ORDER BY', '\n    ORDER BY')\
+        .replace('LIMIT', '\n    LIMIT')\
+        .replace('WHERE', '\n    WHERE')\
+        .replace('AND', '\n    AND')\
+        .replace('LEFT', '\n    LEFT')\
+        .replace('INNER', '\n    INNER')\
+        .replace('INSERT', '\n    INSERT')\
+        .replace('DELETE', '\n    DELETE')
+    return sql
+
 
 class TimerProxy(ConnectionProxy):
     def cursor_execute(self, execute, cursor, statement, parameters, context, executemany):
@@ -12,7 +34,11 @@ class TimerProxy(ConnectionProxy):
         finally:
             total = time.time() - now
             try:
-                log.info("Query: %s" % statement % parameters)
+                log.info(format_sql("Query: %s" % statement % parameters))
             except TypeError:
-                log.info("Query: %s %s" % (statement, parameters))
+                log.info(format_sql("Query: %s %s" % (statement, parameters)))
             log.info("<<<<< TOTAL TIME: %f <<<<<" % total)
+
+
+
+
