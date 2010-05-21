bdd427bed4c0fb28ec4b38db257f66f7b78e5faf
Marcin Kuzminski <marcin@python-works.com>
2010-05-21 23:20:31 +0200

    removed file controller

--- pylons_app/tests/functional/test_file.py
+++
@@ -1,7 +0,0 @@
-from pylons_app.tests import *
-
-class TestFileController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='file', action='index'))
-        # Test response...
