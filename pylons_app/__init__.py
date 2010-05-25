fca4bb83637dc7093d4ee21b007cf2d435a83572
Marcin Kuzminski <marcin@python-works.com>
2010-05-26 00:20:03 +0200

    templating update, improved look & feel, version bump

--- pylons_app/__init__.py
+++ pylons_app/__init__.py
@@ -2,7 +2,7 @@
 Hg app, a web based mercurial repository managment based on pylons
 """
 
-VERSION = (0, 7, 4, 'beta')
+VERSION = (0, 7, 5, 'beta')
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
