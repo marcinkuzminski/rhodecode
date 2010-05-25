e164400316ddf59f95be060bcfc6b81d5ab063b7
Marcin Kuzminski <marcin@python-works.com>
2010-05-25 23:41:03 +0200

    cleaned unused imports

--- pylons_app/controllers/hg.py
+++ pylons_app/controllers/hg.py
@@ -4,8 +4,6 @@ import logging
 from operator import itemgetter
 from pylons import tmpl_context as c, request, config
 from pylons_app.lib.base import BaseController, render
-from pylons_app.lib.utils import get_repo_slug
-from pylons_app.model.hg_model import HgModel
 from pylons_app.lib.auth import LoginRequired
 log = logging.getLogger(__name__)
 
