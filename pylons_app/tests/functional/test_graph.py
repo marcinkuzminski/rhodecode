c332b03f531386cf8e593736394c9abe6bc1197e
Marcin Kuzminski <marcin@python-works.com>
2010-06-29 13:51:03 +0200

    removed unneeded graph controller

--- pylons_app/tests/functional/test_graph.py
+++
@@ -1,7 +0,0 @@
-from pylons_app.tests import *
-
-class TestGraphController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='graph', action='index'))
-        # Test response...
