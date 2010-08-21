d72dd25a7ae3ca704c6cddce29d8f22e13c9643f
Marcin Kuzminski <marcin@python-works.com>
2010-08-21 18:32:53 +0200

    version bump

--- pylons_app/__init__.py
+++ pylons_app/__init__.py
@@ -23,7 +23,7 @@ Hg app, a web based mercurial repository managment based on pylons
 @author: marcink
 """
 
-VERSION = (0, 8, 1, 'beta')
+VERSION = (0, 8, 2, 'beta')
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
