a88e5be119448bf0cd4bef4a3051ae1ee7833a8f
Marcin Kuzminski <marcin@python-works.com>
2010-09-22 04:31:21 +0200

    some code fixes templates+helpers new rfc date without tz, added timerproxy formatting
    --HG--
    branch : celery

--- pylons_app/lib/helpers.py
+++ pylons_app/lib/helpers.py
@@ -325,6 +325,7 @@ isodate = lambda  x: util.datestr(x, '%Y-%m-%d %H:%M %1%2')
 isodatesec = lambda  x: util.datestr(x, '%Y-%m-%d %H:%M:%S %1%2')
 localdate = lambda  x: (x[0], util.makedate()[1])
 rfc822date = lambda  x: util.datestr(x, "%a, %d %b %Y %H:%M:%S %1%2")
+rfc822date_notz = lambda  x: util.datestr(x, "%a, %d %b %Y %H:%M:%S")
 rfc3339date = lambda  x: util.datestr(x, "%Y-%m-%dT%H:%M:%S%1:%2")
 time_ago = lambda x: util.datestr(_age(x), "%a, %d %b %Y %H:%M:%S %1%2")
 
