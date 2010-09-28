44cc9c2f8187d7942b766ca8fc80bc5ecbcf6f92
Marcin Kuzminski <marcin@python-works.com>
2010-09-28 20:26:13 +0000

    version bump

--- pylons_app/__init__.py
+++ pylons_app/__init__.py
@@ -24,7 +24,7 @@ versioning implementation: http://semver.org/
 @author: marcink
 """
 
-VERSION = (0, 8, 3, 'beta')
+VERSION = (0, 8, 4, 'beta')
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
