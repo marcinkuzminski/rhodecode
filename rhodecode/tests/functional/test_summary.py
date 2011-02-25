a4f6d7afc1841f7675ef7de85a67d015416951d7
Marcin Kuzminski <marcin@python-works.com>
2011-02-25 01:11:00 +0100

    fixed summary tests
    --HG--
    branch : beta

--- rhodecode/tests/functional/test_summary.py
+++ rhodecode/tests/functional/test_summary.py
@@ -1,4 +1,6 @@
 from rhodecode.tests import *
+from rhodecode.model.db import Repository
+from rhodecode.lib.utils import invalidate_cache
 
 class TestSummaryController(TestController):
 
@@ -11,9 +13,20 @@ class TestSummaryController(TestController):
         assert """<img style="margin-bottom:2px" class="icon" title="public repository" alt="public repository" src="/images/icons/lock_open.png"/>""" in response.body
 
         #codes stats
+
+
+        self._enable_stats()
+        invalidate_cache('get_repo_cached_%s' % HG_REPO)
+        response = self.app.get(url(controller='summary', action='index', repo_name=HG_REPO))
         assert """var data = {"Python": 42, "Rst": 11, "Bash": 2, "Makefile": 1, "Batch": 1, "Ini": 1, "Css": 1};""" in response.body, 'wrong info about % of codes stats'
 
         # clone url...
         assert """<input type="text" id="clone_url" readonly="readonly" value="hg clone http://test_admin@localhost:80/%s" size="70"/>""" % HG_REPO in response.body
 
+
+    def _enable_stats(self):
+        r = Repository.by_repo_name(HG_REPO)
+        r.enable_statistics = True
+        self.sa.add(r)
+        self.sa.commit()
 
