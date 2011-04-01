cc5227a5d6f8efce5e60332dca61cdebc80f4c20
Marcin Kuzminski <marcin@python-works.com>
2011-04-02 00:55:26 +0200

    fixes for issue #148
    --HG--
    branch : beta

--- rhodecode/lib/utils.py
+++ rhodecode/lib/utils.py
@@ -165,7 +165,7 @@ def get_repos(path, recursive=False):
             cur_path = os.path.join(p, dirpath)
             try:
                 scm_info = get_scm(cur_path)
-                yield scm_info[1].split(path)[-1].lstrip('/'), scm_info
+                yield scm_info[1].split(path)[-1].lstrip(os.sep), scm_info
             except VCSError:
                 if not recursive:
                     continue
