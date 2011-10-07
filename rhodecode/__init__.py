3be2855ae44afdfcfc2089772a9ed235ffdd88cf
Marcin Kuzminski <marcin@python-works.com>
2011-10-07 19:59:47 +0200

    version bump
    --HG--
    branch : beta

--- rhodecode/__init__.py
+++ rhodecode/__init__.py
@@ -25,9 +25,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import platform
 
-VERSION = (1, 2, 0, 'beta')
+VERSION = (1, 3, 0, 'beta')
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
-__dbversion__ = 3 #defines current db version for migrations
+__dbversion__ = 4 #defines current db version for migrations
 __platform__ = platform.system()
 __license__ = 'GPLv3'
 
