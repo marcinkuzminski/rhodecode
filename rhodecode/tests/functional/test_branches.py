54804291e10668e51ecdf1ab478ffa3ab8aaba08
Marcin Kuzminski <marcin@python-works.com>
2011-12-18 04:41:52 +0200

    test fixes
    --HG--
    branch : beta

--- rhodecode/tests/functional/test_branches.py
+++ rhodecode/tests/functional/test_branches.py
@@ -6,10 +6,9 @@ class TestBranchesController(TestController):
         self.log_user()
         response = self.app.get(url(controller='branches',
                                     action='index', repo_name=HG_REPO))
-
-        self.assertTrue("""<a href="/%s/changeset/27cd5cce30c96924232dffcd24178a07ffeb5dfc">default</a>""" % HG_REPO in response.body)
-        self.assertTrue("""<a href="/%s/changeset/97e8b885c04894463c51898e14387d80c30ed1ee">git</a>""" % HG_REPO in response.body)
-        self.assertTrue("""<a href="/%s/changeset/2e6a2bf9356ca56df08807f4ad86d480da72a8f4">web</a>""" % HG_REPO in response.body)
+        response.mustcontain("""<a href="/%s/files/27cd5cce30c96924232dffcd24178a07ffeb5dfc/">default</a>""" % HG_REPO)
+        response.mustcontain("""<a href="/%s/files/97e8b885c04894463c51898e14387d80c30ed1ee/">git</a>""" % HG_REPO)
+        response.mustcontain("""<a href="/%s/files/2e6a2bf9356ca56df08807f4ad86d480da72a8f4/">web</a>""" % HG_REPO)
 
 
 
--- rhodecode/tests/functional/test_home.py
+++ rhodecode/tests/functional/test_home.py
@@ -1,22 +1,22 @@
 from rhodecode.tests import *
 
+
 class TestHomeController(TestController):
 
     def test_index(self):
         self.log_user()
         response = self.app.get(url(controller='home', action='index'))
         #if global permission is set
-        self.assertTrue('ADD NEW REPOSITORY' in response.body)
-        self.assertTrue('href="/%s/summary"' % HG_REPO in response.body)
-        # Test response...
+        response.mustcontain('ADD REPOSITORY')
+        response.mustcontain('href="/%s/summary"' % HG_REPO)
 
-        self.assertTrue("""<img class="icon" title="Mercurial repository" """
+        response.mustcontain("""<img class="icon" title="Mercurial repository" """
                         """alt="Mercurial repository" src="/images/icons/hg"""
-                        """icon.png"/>""" in response.body)
-        self.assertTrue("""<img class="icon" title="public repository" """
+                        """icon.png"/>""")
+        response.mustcontain("""<img class="icon" title="public repository" """
                         """alt="public repository" src="/images/icons/lock_"""
-                        """open.png"/>""" in response.body)
-        
-        self.assertTrue("""<a title="Marcin Kuzminski &lt;marcin@python-works.com&gt;
-merge" class="tooltip" href="/vcs_test_hg/changeset/27cd5cce30c96924232dffcd24178a07ffeb5dfc">r173:27cd5cce30c9</a>"""
-                            in response.body)
+                        """open.png"/>""")
+
+        response.mustcontain(
+"""<a title="Marcin Kuzminski &lt;marcin@python-works.com&gt;:\n
+merge" class="tooltip" href="/vcs_test_hg/changeset/27cd5cce30c96924232dffcd24178a07ffeb5dfc">r173:27cd5cce30c9</a>""")
