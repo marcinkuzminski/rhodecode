3035a4f72293a4847079104e44dcb2b28142c35d
Marcin Kuzminski <marcin@python-works.com>
2012-01-04 03:21:36 +0200

    added requires.txt file
    --HG--
    branch : beta

--- setup.py
+++ setup.py
@@ -3,31 +3,13 @@ from rhodecode import get_version
 from rhodecode import __platform__
 from rhodecode import __license__
 from rhodecode import PLATFORM_OTHERS
+from rhodecode import requirements
 
 py_version = sys.version_info
 
 if py_version < (2, 5):
     raise Exception('RhodeCode requires python 2.5 or later')
 
-requirements = [
-        "Pylons==1.0.0",
-        "Beaker==1.6.2",
-        "WebHelpers>=1.2",
-        "formencode==1.2.4",
-        "SQLAlchemy==0.7.3",
-        "Mako==0.5.0",
-        "pygments>=1.4",
-        "mercurial>=2.0,<2.1",
-        "whoosh<1.8",
-        "celery>=2.2.5,<2.3",
-        "babel",
-        "python-dateutil>=1.5.0,<2.0.0",
-        "dulwich>=0.8.0,<0.9.0",
-        "vcs>=0.2.3.dev",
-        "webob==1.0.8",
-        "markdown==2.0.3",
-        "docutils==0.8.1",
-    ]
 
 dependency_links = [
     "https://secure.rhodecode.org/vcs/archive/default.zip#egg=vcs-0.2.3.dev",
