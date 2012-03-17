973ff1d943e2653332c500450f26b4373b55f633
Marcin Kuzminski <marcin@python-works.com>
2012-03-17 18:29:00 +0200

    bump sqlalchemy version
    --HG--
    branch : beta

--- rhodecode/__init__.py
+++ rhodecode/__init__.py
@@ -41,7 +41,7 @@ requirements = [
     "Beaker==1.6.3",
     "WebHelpers==1.3",
     "formencode==1.2.4",
-    "SQLAlchemy==0.7.5",
+    "SQLAlchemy==0.7.6",
     "Mako==0.6.2",
     "pygments>=1.4",
     "whoosh>=2.3.0,<2.4",
