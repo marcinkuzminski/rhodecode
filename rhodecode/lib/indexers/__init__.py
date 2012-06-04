89b04803e7872228274999ceb2e60ba535ea7c69
Marcin Kuzminski <marcin@python-works.com>
2012-06-05 00:48:20 +0200

    UI fixes for searching
    --HG--
    branch : beta

--- rhodecode/lib/indexers/__init__.py
+++ rhodecode/lib/indexers/__init__.py
@@ -40,7 +40,7 @@ from whoosh.index import create_in, open_dir
 from whoosh.formats import Characters
 from whoosh.highlight import highlight, HtmlFormatter, ContextFragmenter
 
-from webhelpers.html.builder import escape
+from webhelpers.html.builder import escape, literal
 from sqlalchemy import engine_from_config
 
 from rhodecode.model import init_model
@@ -231,7 +231,7 @@ class WhooshResultWrapper(object):
         if self.search_type != 'content':
             return ''
         hl = highlight(
-            text=escape(content),
+            text=content,
             terms=self.highlight_items,
             analyzer=ANALYZER,
             fragmenter=FRAGMENTER,
