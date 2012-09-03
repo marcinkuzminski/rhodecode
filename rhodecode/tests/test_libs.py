77fa9219f9a37d4b29221e3192fc05b90108b961
Marcin Kuzminski <marcin@python-works.com>
2012-09-03 19:47:23 +0200

    more py2.5 compatibility patches
    --HG--
    branch : beta

--- rhodecode/tests/test_libs.py
+++ rhodecode/tests/test_libs.py
@@ -22,7 +22,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+from __future__ import with_statement
 import unittest
 import datetime
 import hashlib
