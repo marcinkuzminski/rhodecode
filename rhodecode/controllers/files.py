6c5338f453fea235a6ba456e256ee063e23d9aa4
Marcin Kuzminski <marcin@python-works.com>
2012-10-20 13:48:13 +0200

    fixed issue with file history for new added files
    --HG--
    branch : beta

--- rhodecode/controllers/files.py
+++ rhodecode/controllers/files.py
@@ -155,7 +155,11 @@ class FilesController(BaseRepoController):
 
             if c.file.is_file():
                 _hist = c.rhodecode_repo.get_changeset().get_file_history(f_path)
-                c.file_changeset = c.changeset if c.changeset.revision < _hist[0].revision else _hist[0]
+                c.file_changeset = c.changeset
+                if _hist:
+                    c.file_changeset = (c.changeset 
+                                        if c.changeset.revision < _hist[0].revision
+                                        else _hist[0])
                 c.file_history = self._get_node_history(None, f_path, _hist)
                 c.authors = []
                 for a in set([x.author for x in _hist]):
