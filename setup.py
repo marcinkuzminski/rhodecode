c695e75749905cb1862ef2ea98a4a3e7ab275661
Marcin Kuzminski <marcin@python-works.com>
2012-11-05 01:04:15 +0100

    bumped mercurial version
    --HG--
    branch : beta
    extra : rebase_source : 6efc9fb44750a3173885479bf087d3d4e54ad338

--- setup.py
+++ setup.py
@@ -60,10 +60,10 @@ if sys.version_info < (2, 7):
     requirements.append("unittest2")
 
 if is_windows:
-    requirements.append("mercurial==2.3.2")
+    requirements.append("mercurial==2.4.0")
 else:
     requirements.append("py-bcrypt")
-    requirements.append("mercurial==2.3.2")
+    requirements.append("mercurial==2.4.0")
 
 
 dependency_links = [
