8cdb1d9ea1398e688346eaabd9c0e700d759411c
Marcin Kuzminski <marcin@python-works.com>
2012-11-29 21:35:08 +0100

    fixed tests
    --HG--
    branch : beta

--- rhodecode/tests/vcs/test_git.py
+++ rhodecode/tests/vcs/test_git.py
@@ -470,9 +470,11 @@ class GitChangesetTest(unittest.TestCase):
         for fname, revision_dict in files.items():
             for rev, data in revision_dict.items():
                 cs = self.repo.get_changeset(rev)
-                ann = cs.get_file_annotate(fname)
 
-                l1 = [x[1].raw_id for x in ann]
+                l1_1 = [x[1] for x in cs.get_file_annotate(fname)]
+                l1_2 = [x[2]().raw_id for x in cs.get_file_annotate(fname)]
+                self.assertEqual(l1_1, l1_2)
+                l1 = l1_1
                 l2 = files[fname][rev]['changesets']
                 self.assertTrue(l1 == l2 , "The lists of revision for %s@rev %s"
                                 "from annotation list should match each other, "
