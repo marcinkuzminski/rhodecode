a63948418f1e580b84cddb8071a5ef8fbe49d769
Marcin Kuzminski <marcin@python-works.com>
2013-04-07 19:14:17 +0200

    fixed docstring on cleanup command

--- rhodecode/lib/paster_commands/cleanup.py
+++ rhodecode/lib/paster_commands/cleanup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-    package.rhodecode.lib.cleanup
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    rhodecode.lib.paster_commands.cleanup
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     cleanup-repos paster command for RhodeCode
 
