32dd3fdf0533c59610b4254d0bdba1c8a9fb4edb
Marcin Kuzminski <marcin@python-works.com>
2013-06-17 20:10:47 +0200

    Added some missing tests for copy+chmod, copy+chmod+modify on diff parser

--- rhodecode/tests/models/test_diff_parsers.py
+++ rhodecode/tests/models/test_diff_parsers.py
@@ -223,6 +223,31 @@ DIFF_FIXTURES = {
           'deleted': 0,
           'binary': True,
           'ops': {COPIED_FILENODE: 'file copied from file1 to file2'}}),
+    ],
+    'hg_diff_copy_and_modify_file.diff': [
+        ('file3', 'M',
+         {'added': 1,
+          'deleted': 0,
+          'binary': False,
+          'ops': {COPIED_FILENODE: 'file copied from file2 to file3',
+                  MOD_FILENODE: 'modified file'}}),
+    ],
+    'hg_diff_copy_and_chmod_file.diff': [
+        ('file4', 'M',
+         {'added': 0,
+          'deleted': 0,
+          'binary': True,
+          'ops': {COPIED_FILENODE: 'file copied from file3 to file4',
+                  CHMOD_FILENODE: 'modified file chmod 100644 => 100755'}}),
+    ],
+    'hg_diff_copy_chmod_and_edit_file.diff': [
+        ('file5', 'M',
+         {'added': 2,
+          'deleted': 1,
+          'binary': False,
+          'ops': {COPIED_FILENODE: 'file copied from file4 to file5',
+                  CHMOD_FILENODE: 'modified file chmod 100755 => 100644',
+                  MOD_FILENODE: 'modified file'}}),
     ]
 #     'large_diff.diff': [
 #         ('.hgignore', 'A', {'deleted': 0, 'binary': False, 'added': 3, 'ops': {1: 'new file 100644'}}),
